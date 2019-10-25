df4190394b8d168f3b1669858196b9e8dad04cbe
BaronLip <baronlip@gmail.com>
2019-10-25 15:12:35 -0500

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,20 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+
+document.addEventListener('DOMContentLoaded', launchApp)
+
+function launchApp() {
+    // console.log("DOM is loaded.")
+    fetch("http://localhost:3000/trainers")
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(json) {
+        console.log(json)
+    })
+    .catch(function(error) {
+        console.log(error);
+    })
+}
